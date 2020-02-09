9fdbf3fe5c929ff18370f516349572bf6cf5396a
urevise <50316882+urevise@users.noreply.github.com>
2020-02-09 21:20:52 +0900

    tab menu

---
+++ tabMenu.js
@@ -0,0 +1,34 @@
+const tabMenu = document.querySelector('.tab-list-wrap');
+const tabItems = tabMenu.querySelectorAll('li');
+const tabSelect = document.querySelector('.tab-cnt-wrap');
+const tabContent = tabSelect.querySelectorAll('.tab-pannel');
+
+function setSelectItem(e) {
+    let clickedList = e.target.parentNode;
+    let clickedIndex = e.target.parentNode.getAttribute('data-tab');
+
+    for (var i = 0; i < tabItems.length; i++) {
+        tabItems[i].classList.remove('active');
+    }
+    for (var j = 0; j < tabContent.length; j++) {
+        tabContent[j].classList.remove('active');
+    }
+    
+    clickedList.classList.add('active');
+    document.getElementById(clickedIndex).classList.add('active');
+}
+
+function tabEvent() {
+    for(var i = 0; i < tabItems.length; i++) {
+        tabItems[i].addEventListener('click', function(e) {
+            e.preventDefault;
+            setSelectItem(e);
+        });
+    }
+}
+
+function init() {
+    tabEvent();
+}
+
+init();
